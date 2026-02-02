31ea1f5050b69fa9672adb678ddac7e332856a76
Ismael <memessr83@gmail.com>
2026-02-02 10:54:11 -0300

    Add initial project files and configuration for Java module

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+        String nome = " Ismael";
+        int idade = 18;
+
+        System.out.println("Olá, meu nome é" + nome);
+        System.out.println("E tenho " + idade + " anos.");
+        System.out.println("Esse é o meu primeiro código em Java!");
+
+    }
+}
